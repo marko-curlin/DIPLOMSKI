3765ae36882050d0a962d3ca6c7eb4c4c343e5bf
Marko Curlin <marko.curlin@live.com>
2022-06-17 00:57:46 +0200

    Add publishing of power and yaw (deg) to u_turn.py

--- u_turn.py
+++ u_turn.py
@@ -2,23 +2,57 @@ from math import radians, degrees
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist, Vector3
+from std_msgs.msg import Float32
 import util
 
 yaw = 0
+yaw_publisher: rospy.Publisher
+
+voltage, current = 0, 0
+power_publisher: rospy.Publisher
 
 
-def callback(odom_data):
+def write_voltage(voltage_data):
+    global voltage
+    # rospy.logdebug("Received data from voltage topic")
+    voltage = voltage_data.data
+
+
+def write_current_and_print(current_data):
+    global current
+    # rospy.logdebug("Received data from current topic")
+    current = current_data.data
+    power = voltage*current
+    print(f"power: {power}")
+
+    power_data = Float32()
+    power_data.data = power
+    power_publisher.publish(power_data)
+
+
+def odom_callback(odom_data):
     # rospy.logdebug("Received data from /viv/viv_velocity_controller/odom")
-    global yaw  # what is the value rad or deg
-    yaw = util.get_yaw_from_pose_message(odom_data.pose.pose) # first pose is PoseWithCovariance
+    global yaw  # radians
+    yaw = util.get_yaw_from_pose_message(odom_data.pose.pose)  # first pose is PoseWithCovariance
+
+    yaw_data = Float32()
+    yaw_data.data = degrees(yaw)
+    yaw_publisher.publish(yaw_data)
 
 
 def rotate():
+    global yaw_publisher, power_publisher
     # Starts a new node
     rospy.init_node('u_turn')
 
+    yaw_publisher = rospy.Publisher('/T265/camera/odom/yaw', Float32, queue_size=None)
+
     velocity_publisher = rospy.Publisher('/viv/marko/cmd_vel', Twist, queue_size=None)
-    rospy.Subscriber('/T265/camera/odom/sample', Odometry, callback)
+    rospy.Subscriber('/T265/camera/odom/sample', Odometry, odom_callback)
+
+    rospy.Subscriber('/battery_voltage_sensor_reading', Float32, write_voltage)
+    rospy.Subscriber('/battery_current_sensor_reading', Float32, write_current_and_print)
+    power_publisher = rospy.Publisher('/battery_power_sensor_calculation', Float32, queue_size=None)
 
     # Receiving the user's input
     print("Let's make a U-turn")
